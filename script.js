5b85525e99bd07197e0f604954e165941ea3ca40
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-07-02 19:51:39 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false
+let firstCard, secondCard;
+
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true
+        firstCard = this;
+        return;
+    }
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+}
+
+
+
+function checkForMatch() {
+    // do cards match?
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards() {
+    // match detected
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    // no match
+    lockBoard = true
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+resetBoard();
+    }, 1000);
+}
+
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+
+cards.forEach(card => card.addEventListener('click', flipCard))
